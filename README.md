713fe59031ff0cc58b49ee7dcf2ee86b5c64bdb2
tsimms <tsimms@users.noreply.github.com>
2023-03-23 17:21:11 -0400

    Initial load: 2 files modified, 6 added

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# node-sudoku
+# sudoku
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-jgcjhu)
+A Sudoku solver
+
+This app was meant to help solve Sudoku puzzles. It uses three strategies for selecting definitive number choices,
+and once exhausted, will apply a brute force algorithm to test potential permutations until failure or success.
+
+To run:
+
+```
+npm start [board_filename]
+```
+
+...where `board_filename` is a path to the JSON file containing the board.
--- index.js
+++ index.js
@@ -1,3 +1,61 @@
-// run `node index.js` in the terminal
+const Board = require('./src/board');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const boardFile = process.argv[2];
+if (!boardFile) {
+  console.error(
+    'Board file not found. Please specify path to file containing the board.'
+  );
+  process.exit();
+}
+const board = new Board();
+board.load(boardFile);
+
+const nextMove = (col, row, val) => {
+  if (!board.get(col, row).isSet()) {
+    board.set(col, row, val);
+    return true;
+  }
+};
+
+let repeat = true;
+while (repeat) {
+  repeat = false;
+  board.setPossible();
+  if (board.getAbsolutes().length > 0) {
+    console.log('Absolutes found.');
+    board.setAbsolutes();
+    repeat = true;
+  } else {
+    console.log('Ambiguous. Checking other empties.');
+    if (board.checkOtherEmpties()) repeat = true;
+  }
+  if (!board.hasFailed()) {
+    console.log('Board still alive!');
+  } else {
+    console.log('Uh oh... borked.');
+    repeat = false;
+    return;
+  }
+  console.log(board.show());
+
+  // Add solution steps here
+  //if (!repeat && nextMove(col,row,number))
+  //   repeat = true;
+
+  if (!repeat) {
+    const list = board
+      .elements()
+      .map((r, y) => r.map((e, x) => ({ ...e, col: x + 1, row: y + 1 })))
+      .flat()
+      .filter((e) => !e.isSet())
+      .map((e) => ({
+        coord: `(${e.col},${e.row})`,
+        element: e.element,
+        possible: e.possible,
+      }));
+
+    console.log(list);
+  }
+  console.log(board.show());
+  console.log(board.getEmptyCount());
+}
